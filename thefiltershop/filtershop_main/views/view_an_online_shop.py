db2dac045e4c929942059b546c1be7c505694d51
TechNech <technech.vst@gmail.com>
2023-10-08 01:48:36 +0500

    fixed online shops index, listed games for single shop

--- thefiltershop/filtershop_main/views/view_an_online_shop.py
+++ thefiltershop/filtershop_main/views/view_an_online_shop.py
@@ -6,9 +6,14 @@ from ..models import Filter
  
 def online_shop(request, shop_id):
     a_shop = get_object_or_404(Online_Shop, pk=shop_id)
+    # links_to_shops = a_shop.on_shop.all()
     negative_filters = Filter.objects.filter(valueforfilter__for_entity__pk = shop_id, valueforfilter__filter__is_positive=False)
     positive_filters = Filter.objects.filter(valueforfilter__for_entity__pk = shop_id,  valueforfilter__filter__is_positive=True)
-    
-    return render(request, "thefiltershop/online_shop.html", {"a_shop": a_shop,
-                                                       "negative_filters": negative_filters, "positive_filters": positive_filters})
+
+    return render(request, "thefiltershop/online_shop.html", {
+                                                       "a_shop": a_shop,
+                                                       "negative_filters": negative_filters, 
+                                                       "positive_filters": positive_filters,
+                                                    #    "links_to_shops": links_to_shops
+                                                       })
     
