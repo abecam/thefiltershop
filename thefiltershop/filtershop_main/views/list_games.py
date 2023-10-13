8de86087300cfe77a7c62399733fec86db83bff3
Alain <abecam@gmail.com>
2023-10-13 19:35:52 +0200

    Added filter for categories for all lists of games. Unfortunately that also meant bearking the list of game partial to one partial for one type of game, as the links are built into the template...

--- thefiltershop/filtershop_main/views/list_games.py
+++ thefiltershop/filtershop_main/views/list_games.py
@@ -27,38 +27,56 @@ def get_artisans_games(request):
 
 
 def get_indies_games(request):
+    game_categories = Game_Category.objects.all()
+    category_id = request.GET.get('category_id')  # Get the selected category ID from the query parameters
+    
     list_of_games_indies = get_all_games_for_size(Studio.SizeInPersons.INDIE)
 
-    paginator = Paginator(list_of_games_indies, 25)  # Show 25 contacts per page.
+    # Apply category filtering if a category is selected
+    if category_id:
+        list_of_games_indies = list_of_games_indies.filter(categories__id=category_id)
+        
+    paginator = Paginator(list_of_games_indies, 8)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    context = {"page_obj": page_obj}
+    context = {"page_obj": page_obj, "categories": game_categories, "selected_category": category_id}
 
     return render(request, "thefiltershop/indies_games.html", context)
 
 def get_artisans_and_indies_games_that_made_it(request):
+    game_categories = Game_Category.objects.all()
+    category_id = request.GET.get('category_id')  # Get the selected category ID from the query parameters
+    
     list_of_games_that_made_it = get_all_games_that_made_it()
+    
+    # Apply category filtering if a category is selected
+    if category_id:
+        list_of_games_that_made_it = list_of_games_that_made_it.filter(categories__id=category_id)
 
     paginator = Paginator(list_of_games_that_made_it, 25)  # Show 25 contacts per page.
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    context = {"page_obj": page_obj}
+    context = {"page_obj": page_obj, "categories": game_categories, "selected_category": category_id}
     
     return render(request, "thefiltershop/they_made_it.html", context)
 
 def get_best_of_the_rest(request):
-    list_of_best_of_the_rest = get_all_best_of_the_rest()
-
+    game_categories = Game_Category.objects.all()
+    category_id = request.GET.get('category_id')  # Get the selected category ID from the query parameters
+    
+    # get_all_best_of_the_rest return a list, so the filtering for category needs to be done before.
+    list_of_best_of_the_rest = get_all_best_of_the_rest(category_id)
+        
     paginator = Paginator(list_of_best_of_the_rest, 25)  # Show 25 contacts per page.
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
 
-    context = {"page_obj": page_obj}
+    context = {"page_obj": page_obj, "categories": game_categories, "selected_category": category_id}
 
     return render(request, "thefiltershop/best_of_the_rest.html", context)
 
@@ -77,15 +95,20 @@ def get_all_games_for_size(max_size_of_studio) :
 def get_all_games_that_made_it() :
     # Here we don't care about the Publisher size
     all_for_size = Videogame_common.objects.filter(Q(studios__size_of_studio =  Studio.SizeInPersons.ARTISAN) | Q(studios__size_of_studio =  Studio.SizeInPersons.INDIE), ~Q(they_have_made_it = Videogame_common.TheyHaveMadeIt.NO))
-    print(all_for_size.query)
     
     return all_for_size
 
-def get_all_best_of_the_rest() :
+def get_all_best_of_the_rest(for_category) :
     # On all filtered games, find which one would actually be good.
     # Best of the rest: gameplay_rating > 80 & good_wo_iap > 80 & good_wo_ads > 80 & use_psycho_tech == 0
     # ! -1 for good_wo_iap or good_wo_ads or use_psycho_tech means that they don't use it at all!
-    all_filtered_games = Videogame_common.objects.annotate(number_of_filters=Count('valueforfilter', filter=Q(valueforfilter__filter__is_positive=False))).exclude( number_of_filters = 0).order_by("crapometer")[:100]
+    if for_category is not None :
+        # Apply category filtering if a category is selected
+        pre_filterd_games = Videogame_common.objects.filter (categories__id=for_category)
+        all_filtered_games = pre_filterd_games.annotate(number_of_filters=Count('valueforfilter', filter=Q(valueforfilter__filter__is_positive=False))).exclude( number_of_filters = 0).order_by("crapometer")[:100]
+    else :
+        all_filtered_games = Videogame_common.objects.annotate(number_of_filters=Count('valueforfilter', filter=Q(valueforfilter__filter__is_positive=False))).exclude( number_of_filters = 0).order_by("crapometer")[:100]
+        
     # And exclude all that don't respect the rules
     remaining_games = []
     nb_of_remaing_games = 0
