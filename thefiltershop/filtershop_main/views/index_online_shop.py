277094721b99219aeebf223606fd2ec9d1c79ee1
Alain Becam <abecam@gmail.com>
2025-02-23 19:03:21 +0100

    Initial transformation toward the rope -> remove all things excepted videogames (and applications) Only on views now. WIP adaptation of the CSS

--- thefiltershop/filtershop_main/views/index_online_shop.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from ..models import Online_Shop
-
-def index_online_shops(request):
-    latest_shops = Online_Shop.objects.order_by("-date_creation")[:5]
-    context = {"latest_shops": latest_shops}
-    return render(request, "thefiltershop/index_online_shops.html", context)
--- thefiltershop/filtershop_main/views/index_physical_shop.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-
-from ..models import Physical_shop
-
-def index_physical_shops(request):
-    latest_shops = Physical_shop.objects.order_by("-date_creation")[:5]
-    context = {"latest_shops": latest_shops}
-    return render(request, "thefiltershop/index_physical_shops.html", context)
